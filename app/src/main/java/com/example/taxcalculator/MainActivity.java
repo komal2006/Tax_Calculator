a0b9633791f5d2c837334cd0d55a5e094106b16c
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 13:04:54 -0500

    Button in main Activity

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -94,23 +94,50 @@ public class MainActivity extends AppCompatActivity
 
         // button to navigate to next activity
         btnShow.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Double grossIncome = Double.parseDouble(edtGrossIncome.getText().toString());
-//                Double rrsp = Double.parseDouble(edtRRSPcontri.getText().toString());
-//                customer = new CRACustomer(edtSinNumber.getText().toString(),
-//                        edtFirstName.getText().toString(),
-//                        edtLastName.getText().toString(),
-//                        selectedGender, grossIncome, rrsp);
-//                Intent mIntent = new Intent(MainActivity.this, TaxCalculated.class);
-//                mIntent.putExtra("CRACustomer", customer);
-//                startActivity(mIntent);
-//            }
-//        });
+            @Override
+            public void onClick(View v) {
+                Double grossIncome = Double.parseDouble(edtGrossIncome.getText().toString());
+                Double rrsp = Double.parseDouble(edtRRSPcontri.getText().toString());
+                customer = new CRACustomer(edtSinNumber.getText().toString(),
+                        edtFirstName.getText().toString(),
+                        edtLastName.getText().toString(),
+                        selectedGender, grossIncome, rrsp);
+                Intent mIntent = new Intent(MainActivity.this, TaxCalculated.class);
+                mIntent.putExtra("CRACustomer", customer);
+                startActivity(mIntent);
+            }
+        });
 
         private void dateFormat() {
             String myFormat = "dd-MMM-yyyy"; //In which you need put here
             java.text.SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
             txtBirthDate.setText(sdf.format(calendar.getTime()));
+        }
+
+        private  void currentDate(){
+            // for tax filing date
+
+            String todayDateFormat = "EEE, MMM d, yyyy";
+            java.text.SimpleDateFormat td = new SimpleDateFormat(todayDateFormat, Locale.CANADA);
+            txtTaxFilingDate.setText(td.format(calendar.getTime()));
+
+        }
+        public  void checkedButton(View view){
+            int radioId = rgGender.getCheckedRadioButtonId();
+            rgGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    if(checkedId == R.id.rdMale){
+                        selectedGender = rdMale.getText().toString();
+                    }else if(checkedId == R.id.rdFemale){
+                        selectedGender = rdFemale.getText().toString();
+                    }else {
+                        selectedGender = rdOther.getText().toString();
+                    }
+                }
+            });
+            radioGender = findViewById(radioId);
+            txtGender.setText(selectedGender);
+            Toast.makeText(this, "Gender:" + radioGender.getText(), Toast.LENGTH_SHORT).show();
         }
     }
