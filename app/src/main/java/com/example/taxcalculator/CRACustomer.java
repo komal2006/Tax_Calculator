ce39462f3ab7cd2a704be1b66ee9b1d9f4a3ad92
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 13:06:46 -0500

    initialization in CRACustomer

--- app/src/main/java/com/example/taxcalculator/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculator/CRACustomer.java
@@ -13,4 +13,16 @@ public class CRACustomer
     double grossIncome, federalTax, provicialTax, empInsurance;
     double rrspContri, rrspCarryForward, taxableIncome, taxPaid;
 
+
+    public CRACustomer(String sinNumber, String firstName,
+                       String lastName, String gender, double grossIncome, double rrspContri)
+    {
+        this.sinNumber = sinNumber;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.fullName = fullName;
+        this.gender = gender;
+        this.grossIncome = grossIncome;
+        this.rrspContri = rrspContri;
+    }
 }
