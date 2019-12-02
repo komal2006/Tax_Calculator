6930ac5890387199758e5b64126217a9e068c701
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 13:05:58 -0500

    decalre variable in CRACustomer

--- app/src/main/java/com/example/taxcalculator/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculator/CRACustomer.java
@@ -5,6 +5,12 @@ package com.example.taxcalculator;
 
 import java.util.Date;
 
-public class CRACustomer {
+public class CRACustomer
+{
+    String sinNumber, firstName, lastName, fullName, gender;
+    Date birthDate,filingDate;
+    int age;
+    double grossIncome, federalTax, provicialTax, empInsurance;
+    double rrspContri, rrspCarryForward, taxableIncome, taxPaid;
 
 }
