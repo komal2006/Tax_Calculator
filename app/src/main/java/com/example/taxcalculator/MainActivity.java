6a510d3b951a2e2d7bcd3410f510171726881e4a
Komal2006 <komaldeepkr99@gmail.com>
2019-12-01 23:32:39 -0500

    coding in main activity

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -6,9 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    private EditText sin_number;
+    private EditText edtFirstName;
+    private EditText edtLastName;
+    private EditText mainAge;
+    private RadioGroup rgMain;
+    private RadioButton rbMale;
+    private RadioButton rbFMale;
+    private RadioButton rbOthers;
+    String radio = "";
+
     private Button btnCalculate;
 
     @Override
