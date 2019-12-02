104538fb4e9400cb30d474beb94b83803d7afd93
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 00:05:51 -0500

    constructor in CRACustomer

--- app/src/main/java/com/example/taxcalculator/CRACustomer.java
+++ app/src/main/java/com/example/taxcalculator/CRACustomer.java
@@ -9,4 +9,23 @@ public class CRACustomer implements Parcelable
     String fName,lName;
     String fullName;
     String gender;
+
+    public String getGender()
+    {
+        return gender;
+    }
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+    public CRACustomer(String sinNumber, String age, String fName, String lName, String gender)
+    {
+        this.sinNumber = sinNumber;
+        this.age = age;
+        this.fName = fName;
+        this.lName = lName;
+        this.fullName = fullName;
+        this.gender = gender;
+    }
+
 }
