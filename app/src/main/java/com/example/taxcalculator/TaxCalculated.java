213aba7014f75f91be227e4ae17ac4172e11b997
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 13:10:15 -0500

    declare variable in tax Calculated class

--- app/src/main/java/com/example/taxcalculator/TaxCalculated.java
+++ app/src/main/java/com/example/taxcalculator/TaxCalculated.java
@@ -10,5 +10,11 @@ import android.widget.TextView;
 
 public class TaxCalculated extends AppCompatActivity {
 
-
+    CRACustomer customer;
+    TextView txtDsin, txtDfullName, txtDgender, txtDgrossIncome,
+            txtDtaxFilingDate, txtDfederalTax, txtDprovincialTax, lblcpp,
+            lblEmpInsurance, lblRRSPcontri, lblCfRRSP,
+            lblTaxableIncome, lblTaxPaid;
+    double cpp = 0, ei = 0;  double rrsp = 0, rrspCf = 0, taxableIncome, federalTax,
+            provincialTax, totalTaxPaid;
 }
