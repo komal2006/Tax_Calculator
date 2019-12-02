0fad32d97a319004fb8feae90ef73c574e755c3f
Komal2006 <komaldeepkr99@gmail.com>
2019-12-02 12:59:34 -0500

    main Activity

--- app/src/main/java/com/example/taxcalculator/MainActivity.java
+++ app/src/main/java/com/example/taxcalculator/MainActivity.java
@@ -1,106 +1,41 @@
 package com.example.taxcalculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
 {
 
-    private EditText sin_number;
-    private EditText edtFirstName;
-    private EditText edtLastName;
-    private EditText txtAge;
-    private RadioGroup rgMain;
-    private RadioButton rbMale;
-    private RadioButton rbFMale;
-    private RadioButton rbOthers;
-    String radio = "";
 
-    private Button btnCalculate;
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        btnCalculate = findViewById(R.id.btnCalculate);
+        CRACustomer customer;
+        final Calendar calendar = Calendar.getInstance();
+        DatePickerDialog datePickerDialog;
+        private TextView txtTitle;
+        private EditText edtSinNumber, edtFirstName, edtLastName,txtBirthDate,edtGrossIncome, edtRRSPcontri;
+        private TextView txtFullName,txtGender, txtTaxFilingDate;
+        private RadioGroup rgGender;
+        private RadioButton radioGender,rdMale,rdFemale,rdOther;
+        private Button btnShow;
+        private String selectedGender = "";
 
 
-        sin_number = findViewById(R.id.sin_number);
-        edtLastName = findViewById(R.id.edtLastName);
-        txtAge = findViewById(R.id.txtAge);
-        rgMain = findViewById(R.id.rgGender);
-        rbMale = findViewById(R.id.rbMale);
-        rbFMale = findViewById(R.id.rbFemale);
-        rbOthers = findViewById(R.id.rbOthers);
-        edtFirstName = findViewById(R.id.edtFirstName);
-        radioAction();
-        calculate();
-
-
+       
     }
-
-    public void radioAction()
-    {
-        rgMain.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId)
-            {
-
-                if(checkedId==R.id.rbMale)
-                {
-                    radio = rbMale.getText().toString();
-                    Toast.makeText(getApplicationContext(),"Male",Toast.LENGTH_SHORT).show();
-                    rbMale.setSelected(true);
-                }
-                else if(checkedId==R.id.rbFemale)
-                {
-
-                    radio = rbFMale.getText().toString();
-                    Toast.makeText(getApplicationContext(),"Female",Toast.LENGTH_SHORT).show();
-                    rbFMale.setSelected(true);
-
-                }
-                else if(checkedId==R.id.rbOthers)
-                {
-
-                    radio = rbOthers.getText().toString();
-                    Toast.makeText(getApplicationContext(),"Others",Toast.LENGTH_SHORT).show();
-                    rbOthers.setSelected(true);
-
-                }
-            }
-        });
-    }
-
-    public void calculate()
-    {
-
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-
-            {
-
-
-                CRACustomer cra = new CRACustomer(sin_number.getText().toString(),txtAge.getText().toString(), edtFirstName.getText().toString(), edtLastName.getText().toString(),rgMain.toString());
-
-                Intent mIntent = new Intent(MainActivity.this, TaxCalculated.class);
-                mIntent.putExtra("CRACustomer",cra);
-                          // mIntent.putExtra("gender", radio);
-                startActivity(mIntent);
-
-            }
-        });
-    }
-}
--- app/src/main/java/com/example/taxcalculator/TaxCalculated.java
+++ app/src/main/java/com/example/taxcalculator/TaxCalculated.java
@@ -2,13 +2,13 @@ package com.example.taxcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class TaxCalculated extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tax_calculated);
-    }
+
 }
